3eedc1dd1eb3c43c55f81aead7d4211dc2823007
Zhouyu Qian <qzy@qzy.io>
2018-05-10 22:16:53 -0700

    Move pretty-printing states to Den module

--- main/Main.hs
+++ main/Main.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE GADTs #-}
 module Main where
 
-import Data.Bifunctor
 import Data.Foldable
-import Prob.CoreAST
 import Prob.Den
 import Prob.Parse
 import System.Environment
@@ -15,17 +13,4 @@ main = do
     mp <- doParseFromFile f
     case mp of
       Nothing -> pure ()
-      Just (Prog p) -> do
-        let r :: [(String, Rational)]
-            r = case p of
-                  Return s e -> map (first show) (denProgReturn s e)
-                  ReturnAll s -> map (first show) (denProgReturnAll s)
-            maxLen1 = maximum (length . fst <$> r)
-            maxLen2 = maximum (length . show . snd <$> r)
-        putStr (replicate maxLen1 '-')
-        putStr " "
-        putStrLn (replicate maxLen2 '-')
-        forM_ r $ \(c, v) -> do
-          putStr c
-          putStr (replicate (maxLen1 - length c + 1) ' ')
-          print v
+      Just (Prog p) -> putStr (denProgPretty p [])
--- src/Prob/Den.hs
+++ src/Prob/Den.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StrictData #-}
 module Prob.Den
   ( denExpr
   , denStmt
   , denProg
+  , denProgPretty
   , denProgReturn
   , denProgReturnAll
   ) where
@@ -126,6 +128,33 @@ denProgReturnAll s =
 denProg :: (Show vt, Ord vt) => Prog r vt -> [(r, Rational)]
 denProg (s `Return` e) = denProgReturn s e
 denProg (ReturnAll s) = denProgReturnAll s
+
+denProgPretty :: forall vt r. (Show vt, Ord vt) => Prog r vt -> ShowS
+denProgPretty p =
+  case r of
+    [] -> showString "No results produced.\n"
+    _ -> bars . foldr (\(col1, col2) s -> showString col1 . showString (replicate (maxLen1 - length col1) ' ') . showString col2 . showChar '\n' . s) id r
+  where
+    r :: [(String, String)]
+    r =
+      case p of
+        Return s e -> map (bimap show show) (denProgReturn s e)
+        ReturnAll s -> map (bimap pprMap show) (denProgReturnAll s)
+    pprMap :: Sigma vt -> String
+    pprMap =
+      M.foldrWithKey
+        (\var val s ->
+           shows var .
+           showString " ->" .
+           showString
+             (if val
+                then "  true "
+                else " false ") $
+           s)
+        " "
+    maxLen1 = maximum (length . fst <$> r)
+    maxLen2 = maximum (length . snd <$> r)
+    bars = showString (replicate (maxLen1 - 1) '-') . showChar ' ' . showString (replicate maxLen2 '-') . showChar '\n'
 
 renormalize :: Fractional c => [(a, c)] -> [(a, c)]
 renormalize l = map (second (/tot)) l
