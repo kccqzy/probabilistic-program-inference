da2a9ba9068e2547df07c0c7e48d723e9ab098e5
Zhouyu Qian <qzy@qzy.io>
2018-05-14 21:25:08 -0700

    Fix issues with steven's semantics
    Mainly an issue with associativity

--- src/Prob/Den2Ori.hs
+++ src/Prob/Den2Ori.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveTraversable #-}
 -- | Denotational semantics using transitional/accepting semantics.
 module Prob.Den2Ori where
@@ -6,8 +6,6 @@ module Prob.Den2Ori where
 import qualified Data.Map.Strict as M
 import Prob.CoreAST (Dist (..), Expr (..), Sigma)
 import Prob.Den
-
-import Debug.Trace
 
 data Stmt vt
   = Seq (Stmt vt)
@@ -23,12 +21,6 @@ data Stmt vt
   | Skip
   deriving (Show, Eq, Functor, Foldable, Traversable)
 
--- | Transitional semantics with debugging.
-denStmtT' :: (Show vt, Ord vt) => Stmt vt -> Sigma vt -> Sigma vt -> Rational
-denStmtT' s sigma' sigma =
-  let !r = denStmtT s sigma' sigma
-  in trace (concat ["denStmtT ", show s, " ", show (M.toList sigma'), " ", show (M.toList sigma), " = ", show r]) r
-
 -- | Transitional semantics.
 denStmtT :: (Show vt, Ord vt) => Stmt vt -> Sigma vt -> Sigma vt -> Rational
 denStmtT (x :~ Bernoulli theta) sigma' sigma
@@ -39,8 +31,8 @@ denStmtT (x := e) sigma' sigma
   | sigma' == M.insert x (denExpr e sigma) sigma = 1
   | otherwise = 0
 denStmtT (If e s1 s2) sigma' sigma
-  | denExpr e sigma = denStmtT' s1 sigma' sigma
-  | otherwise = denStmtT' s2 sigma' sigma
+  | denExpr e sigma = denStmtT s1 sigma' sigma
+  | otherwise = denStmtT s2 sigma' sigma
 denStmtT (Observe _) sigma' sigma
   | sigma' == sigma = 1
   | otherwise = 0
@@ -49,31 +41,26 @@ denStmtT Skip sigma' sigma
   | otherwise = 0
 denStmtT (While _ _ ) _ _ = error "not yet"
 denStmtT (Seq s1 s2) sigma' sigma =
-  sumOverAll sigma (\tau -> denStmtT' s1 tau sigma * denStmtT' s2 sigma' tau * denStmtA' s2 tau) /
-  sumOverAll sigma (\tau -> denStmtT' s1 tau sigma * denStmtA' s2 tau)
-
--- | Accepting semantics with debugging.
-denStmtA' :: (Show vt, Ord vt) => Stmt vt -> Sigma vt -> Rational
-denStmtA' s sigma =
-  let !r = denStmtA s sigma
-  in trace (concat ["denStmtA ", show s, " ",  show (M.toList sigma), " = ", show r]) r
+  let !numerator = sumOverAll sigma (\tau -> denStmtT s1 tau sigma * denStmtT s2 sigma' tau * denStmtA s2 tau)
+      !denominator = sumOverAll sigma (\tau -> denStmtT s1 tau sigma * denStmtA s2 tau)
+  in numerator / denominator
 
 -- | Accepting semantics.
 denStmtA :: (Show vt, Ord vt) => Stmt vt -> Sigma vt -> Rational
 denStmtA (Observe e) sigma
   | denExpr e sigma = 1
   | otherwise = 0
-denStmtA (Seq s1 s2) sigma = denStmtA' s1 sigma * sumOverAll sigma (\tau -> denStmtT' s1 tau sigma * denStmtA' s2 tau)
+denStmtA (Seq s1 s2) sigma = denStmtA s1 sigma * sumOverAll sigma (\tau -> denStmtT s1 tau sigma * denStmtA s2 tau)
 denStmtA _ _ = 1
 
 sumOverAll :: (Ord vt) => Sigma vt -> (Sigma vt -> Rational) -> Rational
 sumOverAll sigma f = sum (map f (allPossibleStates vars))
   where vars = M.keys sigma
 
-infixr 2 `Seq`
-
-test1 :: Stmt String
-test1 = "c1" :~ Bernoulli 0.5 `Seq` Observe (Var "c1")
+infixl 2 `Seq`
 
 test2 :: Stmt String
 test2 = "c1" :~ Bernoulli 0.5 `Seq` "c2" :~ Bernoulli 0.5 `Seq` Observe (Var "c1")
+
+test2' :: Stmt String
+test2' = "c2" :~ Bernoulli 0.5 `Seq` Observe (Var "c1")
