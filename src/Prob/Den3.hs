c28a2c0f2c75c181a8064cd2816b286c6719c95e
Zhouyu Qian <qzy@qzy.io>
2018-05-15 00:55:35 -0700

    New experimental semantics
    Trying to come up with a Haskell-first design.

---
+++ src/Prob/Den3.hs
@@ -0,0 +1,37 @@
+module Prob.Den3 where
+
+import Control.Monad
+import Data.Bifunctor
+import Data.Foldable
+import qualified Data.Map.Strict as M
+import Prob.CoreAST
+import Prob.Den (denExpr)
+
+-- | The 'P' monad represents results with probabilities.
+newtype P vt a = P (Sigma vt -> [(Rational, (Sigma vt, a))])
+
+instance Functor (P vt) where
+  fmap = liftM
+
+instance Applicative (P vt) where
+  pure a = P $ \s -> [(1, (s, a))]
+  (<*>) = ap
+
+instance Monad (P vt) where
+  P ma >>= f =
+    P $ \s ->
+      (concatMap
+         (\(pa, (s', a)) ->
+            case f a of
+              P b -> map (first (pa *)) (b s'))
+         (ma s))
+
+denStmts :: (Ord vt, Show vt, Foldable t) => t (Stmt vt) -> P vt ()
+denStmts = traverse_ denStmt
+
+denStmt :: (Show vt, Ord vt) => Stmt vt -> P vt ()
+denStmt (x := e) = P $ \s -> [(1, (M.insert x (denExpr e s) s, ()))]
+denStmt (x :~ Bernoulli theta) = P $ \s -> [(theta, (M.insert x True s, ())), (1 - theta, (M.insert x False s, ()))]
+denStmt (Observe e) = P $ \s -> if denExpr e s then [(1, (s, ()))] else []
+denStmt (If e s1 s2) = P $ \s -> let P p = denStmts (if denExpr e s then s1 else s2) in p s
+denStmt loop@(While e s1) = P $ \s -> if denExpr e s then (let P p = denStmts (s1++[loop]) in p s) else [(1, (s, ()))]
