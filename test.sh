a200ad26ce47609eebd26275e1a41889ba33ddbc
Zhouyu Qian <qzy@qzy.io>
2026-07-22 22:21:50 -0400

    Add test cases for random variables in front of a loop body
    These are preparatory steps for the next performance improvement.
    
    Currently, these are somewhat slow, because having irrelevant random variables
    in front of a loop body causes the loop body to be re-analyzed once per state.
    The next performance improvement will come from caching the state.
    
    Also change the test script to report timing information.

--- test.sh
+++ test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 cabal new-build || exit 1
+EXE="$(cabal list-bin prob)"
 
 fail=0
 for f in ./examples/*.txt; do
     if [ -f "$f.out" ]; then
-        if diff -q "$f.out" <(cabal new-run prob -- "$f" 2>&1); then
-            echo PASS "$f"
+        if diff -q "$f.out" <(/usr/bin/time -p -o "$f.time" "$EXE" "$f" 2>&1); then
+            echo -n PASS "$f" " "
+            awk '/real/ { print $2 }' "$f.time"
         else
             echo FAIL "$f"
             fail=1
         fi
+        rm -f "$f.time"
     fi
 done
 
