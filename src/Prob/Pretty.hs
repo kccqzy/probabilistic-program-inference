b8484ccdd4af8a951ca895a59c17a63536a48e43
Zhouyu Qian <qzy@qzy.io>
2026-07-25 12:02:36 -0400

    Improve pretty-printing of numbers
    When a rational number is too complicated, show a decimal approximation.
    
    Also, order the results in descending order.

--- src/Prob/Pretty.hs
+++ src/Prob/Pretty.hs
@@ -11,6 +11,7 @@ import Data.Foldable
 import Data.List
 import Data.Ord
 import Data.Ratio
+import Data.Scientific
 import qualified Data.Set as Set
 import Prob.CoreAST
 import Prob.Den (denProg)
@@ -51,14 +52,34 @@ handleProgPretty p m = formatResult <$> r
     formatResult rr =
       bars .
       foldr
-        (\(col1, col2) s -> col1 . formatRational col2 . showChar '\n' . s)
+        (\(col1, col2) s -> col1 . showString col2 . showChar '\n' . s)
         id
-        (sortBy (comparing snd) rr)
+        formattedRats
       where
-        formatRational rat = shows (numerator rat) . showChar '/' . shows (denominator rat)
+        formattedRats :: [(ShowS, String)]
+        formattedRats = map (second formatRational) (sortOn (Down . snd) rr)
         maxLen1 = case p of
           ReturnAll {} -> sum [ 10 + length (show v)| v <- Set.toList allVars ]
           Return {} -> 6
-        maxLen2 = maximum [ length (formatRational v []) | v <- map snd rr ]
+        maxLen2 = maximum (map (length . snd) formattedRats)
         bars =
           showString (replicate (maxLen1 - 1) '-') . showString "  " . showString (replicate maxLen2 '-') . showChar '\n'
+
+formatRational :: Rational -> String
+formatRational rat = ($ []) $
+  if denominator rat >= 50
+  then
+    formatRationalExact . showString " (≈ " . formatRationalApprox . showChar ')'
+  else
+    formatRationalExact
+  where
+    formatRationalExact = shows (numerator rat) . showChar '/' . shows (denominator rat)
+    formatRationalApprox =
+      if approx == 0 then showChar '0'
+      else showString (formatScientific (if approx < e then Exponent else Fixed) Nothing approx)
+      where
+        e = scientific 1 (-4)
+        approx =
+          case fromRationalRepetendLimited 8 rat of
+            Left (r, _) -> r
+            Right (r, _) -> r
