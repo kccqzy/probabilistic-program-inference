46ca278e429475704db2e3677b725d0701cdadec
Zhouyu Qian <qzy@qzy.io>
2026-07-25 10:10:49 -0400

    Improve pretty-printing in Return mode

--- src/Prob/Pretty.hs
+++ src/Prob/Pretty.hs
@@ -27,9 +27,12 @@ handleProgPretty p m = formatResult <$> r
     r :: IO [(ShowS, Rational)]
     r =
       case p of
-        Return {} -> map (first shows) <$> (case m of ModeDen -> pure (denProg p); ModeEval t -> sampled t p)
+        Return {} -> map (first pprVar) <$> (case m of ModeDen -> pure (denProg p); ModeEval t -> sampled t p)
         ReturnAll {} -> map (first pprMap) <$> (case m of ModeDen -> pure (denProg p); ModeEval t -> sampled t p)
       where
+        pprVar :: Bool -> ShowS
+        pprVar True = showString " true  "
+        pprVar False = showString "false  "
         pprMap :: Sigma vt -> ShowS
         pprMap sigma =
           foldr
@@ -53,7 +56,9 @@ handleProgPretty p m = formatResult <$> r
         (sortBy (comparing snd) rr)
       where
         formatRational rat = shows (numerator rat) . showChar '/' . shows (denominator rat)
-        maxLen1 = sum [ 10 + length (show v)| v <- Set.toList allVars ]
+        maxLen1 = case p of
+          ReturnAll {} -> sum [ 10 + length (show v)| v <- Set.toList allVars ]
+          Return {} -> 6
         maxLen2 = maximum [ length (formatRational v []) | v <- map snd rr ]
         bars =
           showString (replicate (maxLen1 - 1) '-') . showString "  " . showString (replicate maxLen2 '-') . showChar '\n'
