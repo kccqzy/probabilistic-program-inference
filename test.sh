98f006b97489add94cb497113d0c81e0668fc265
Zhouyu Qian <qzy@qzy.io>
2026-07-28 00:07:56 -0400

    Test script: report user time rather than wall clock time

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ for f in ./examples/*.txt; do
     if [ -f "$f.out" ]; then
         if diff -q "$f.out" <(/usr/bin/time -p -o "$f.time" "$EXE" "$f" 2>&1); then
             echo -n PASS "$f" " "
-            awk '/real/ { print $2 }' "$f.time"
+            awk '/user/ { print $2 }' "$f.time"
         else
             echo FAIL "$f"
             fail=1
